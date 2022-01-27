7f00804816aa1dc6b10838ffb8e08a2f5bc5de78
Vallecampo <43384819+Vallecampo@users.noreply.github.com>
2022-01-27 12:35:44 -0500

    Delete Fall 202x directory

--- Fall 202x/Fall 2021/Java/Week12/MyMessage.java
+++
@@ -1,9 +0,0 @@
-public class MyMessage{
-	
-	public String message;
-	
-	public void displayMessage(){
-		System.out.println(message);
-		
-	}
-}
--- Fall 202x/Fall 2021/Java/Week12/PrintThings.java
+++
@@ -1,9 +0,0 @@
-public class PrintThings{
-	
-	public static void main(String[] args){
-	
-		//Construct an instance of myMessage
-		MyMessage m = new MyMessage();
-		m.displayMessage();
-	}
-}
